aa4eed9b56066d8b3b03f2e041521a8881f9eb3f
Internest <69037662+StopInternet@users.noreply.github.com>
2025-03-04 22:55:55 +0900

    初回更新分

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function mains(){
+    //ボックスの位置調整
+    var br_setting_1 = document.getElementById('br_1');
+    var out = "<br>"
+    for(var i=1;i<=45;i++){
+      br_setting_1.innerHTML += out
+    }
+  
+}
